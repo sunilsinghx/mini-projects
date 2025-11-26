1a91b382d3feadcf73a22b3b09fa41e35b8e8218
Sunil Singh <sunilsingh09012@gmail.com>
2025-11-26 15:51:37 +0530

    added changes

--- cafe-shop-map/vite.config.ts
+++ cafe-shop-map/vite.config.ts
@@ -1,17 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import fs from 'fs';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ command }) => {
-  return {
-    plugins: [react()],
-    server: command === 'serve' ? {
-      https: {
-        key: fs.readFileSync(path.resolve(__dirname, 'cert.key')),
-        cert: fs.readFileSync(path.resolve(__dirname, 'cert.crt')),
-      },
-      port: 5173,
-    } : undefined,
-  };
-});
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})

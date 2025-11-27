f5e6ba44d003adc2920e94e40a3b478384f4fc08
Sunil <52168181+sunilsinghx@users.noreply.github.com>
2025-11-27 22:42:17 +0530

    Fix icon URL handling for Leaflet markers

--- cafe-shop-map/src/App.tsx
+++ cafe-shop-map/src/App.tsx
@@ -12,7 +12,7 @@ import markerIcon from "leaflet/dist/images/marker-icon.png";
 import markerShadow from "leaflet/dist/images/marker-shadow.png";
 
 // Fix Leaflet default icon paths
-delete L.Icon.Default.prototype._getIconUrl;
+delete (L.Icon.Default.prototype as any)._getIconUrl;
 
 L.Icon.Default.mergeOptions({
   iconRetinaUrl: markerIcon2x,
